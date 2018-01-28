72552491352c16dafc7bec2673490c422b8d8980
qly612525 <qianly@foxmail.com>
2018-01-28 22:59:37 +0800

    fix tes bug

--- src/container/home/index.test.tsx
+++ src/container/home/index.test.tsx
@@ -2,7 +2,7 @@ import { shallow } from 'enzyme';
 import * as React from 'react';
 import Home from './index';
 
-test('App\'s title should be Todos', function () {
+it('App\'s title should be Todos', function () {
     let app = shallow(<Home />);
     expect(app.text()).toEqual('Home');
 });
